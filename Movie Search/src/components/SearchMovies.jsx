f4c76e40cd6f4ee0aea929b4d95223feb5890b47
Gershom Ndolo <gashadogo@gmail.com>
2023-11-24 21:47:34 +0300

    Finishing touches
    Finishing touches on the simple react movie search app

--- Movie Search/src/components/SearchMovies.jsx
+++ Movie Search/src/components/SearchMovies.jsx
@@ -2,19 +2,25 @@ import React from "react"
 
 export default function SearchMovies() {
 
+    const [query, setQuery] = React.useState("")
+    const [movies, setMovies] = React.useState([])
+
+    const handleInput = (event) => {
+        setQuery(event.target.value)
+    }
+
     const searchMovies = async (e) => {
         e.preventDefault()
         
-        const query = "Jurassic Park"
-
         const url = `https://api.themoviedb.org/3/search/movie?api_key=f0614a0d00be71fe03e3926c63cbd51d&query=${query}&language=en-US&include_adult=false`
 
         try {
             const res = await fetch(url)
             const data = await res.json()
 
-            console.log(data)
-            
+            setMovies(data.results)
+            console.log(movies)
+
         } catch (err) {
             console.log(err)
         }
@@ -22,9 +28,32 @@ export default function SearchMovies() {
 
 
     return (
-        <form className="form" onSubmit={searchMovies}>
-            <input className="input" type="text" name="query" placeholder="e.g Jurassic Park"/>
-            <button type="submit" className="button">Search</button>
-        </form>
+        <>
+            <form className="form" onSubmit={searchMovies}>
+                <input 
+                    className="input" 
+                    type="text" name="query" 
+                    placeholder="e.g Jurassic Park"
+                    value={query}
+                    onChange={handleInput}/>
+                <button type="submit" className="button">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.map(movie => (
+                    <div className="card" key={movie.id}>
+                        <img 
+                            className="card--image" 
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                            alt={`${movie.title} poster`}/>
+                        <div className="card--content">
+                            <h3 className="card--title">{movie.title}</h3>
+                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                            <p><small>RATING: {movie.vote_average}</small></p>
+                            <p className="card--desc">{movie.overview}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
